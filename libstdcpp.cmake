60b2aa6356079588bc6d8082a9f0a804d1bb96a0
Dan Green <danngreen1@gmail.com>
2024-04-18 17:13:59 -0700

    Fix c++ build (always build, even if using static libc/libm)

--- libstdcpp.cmake
+++ libstdcpp.cmake
@@ -11,7 +11,7 @@ set(LIBSTDCPP_C98_SOURCES
 	${libstdcpp}/c++98/hashtable_tr1.cc
 	${libstdcpp}/c++98/ios_failure.cc
 	${libstdcpp}/c++98/ios_init.cc
-	${libstdcpp}/c++98/ios_locale.cc
+	# ${libstdcpp}/c++98/ios_locale.cc
 	${libstdcpp}/c++98/list.cc
 	${libstdcpp}/c++98/list-aux.cc
 	${libstdcpp}/c++98/list-aux-2.cc
@@ -33,35 +33,44 @@ set(LIBSTDCPP_C98_SOURCES
 )
 
 set(LIBSTDCPP_C11_SOURCES
-	${libstdcpp}/c++11/bitmap_allocator.cc
-	${libstdcpp}/c++11/pool_allocator.cc
-	${libstdcpp}/c++11/mt_allocator.cc
+	${libstdcpp}/c++11/chrono.cc
 	${libstdcpp}/c++11/codecvt.cc
-	${libstdcpp}/c++11/complex_io.cc
-	${libstdcpp}/c++11/globals_io.cc
-	${libstdcpp}/c++11/hash_tr1.cc
-	${libstdcpp}/c++11/hashtable_tr1.cc
-	${libstdcpp}/c++11/ios_failure.cc
-	${libstdcpp}/c++11/ios_init.cc
-	${libstdcpp}/c++11/ios_locale.cc
-	${libstdcpp}/c++11/list.cc
-	${libstdcpp}/c++11/list-aux.cc
-	${libstdcpp}/c++11/list-aux-2.cc
-	${libstdcpp}/c++11/list_associated.cc
-	${libstdcpp}/c++11/list_associated-2.cc
-	${libstdcpp}/c++11/locale.cc
-	${libstdcpp}/c++11/locale_init.cc
-	${libstdcpp}/c++11/locale_facets.cc
-	${libstdcpp}/c++11/localename.cc
-	${libstdcpp}/c++11/math_stubs_float.cc
-	${libstdcpp}/c++11/math_stubs_long_double.cc
-	${libstdcpp}/c++11/stdexcept.cc
-	${libstdcpp}/c++11/strstream.cc
-	${libstdcpp}/c++11/tree.cc
-	${libstdcpp}/c++11/istream.cc
-	${libstdcpp}/c++11/istream-string.cc
-	${libstdcpp}/c++11/streambuf.cc
-	${libstdcpp}/c++11/valarray.cc
+	${libstdcpp}/c++11/condition_variable.cc
+	${libstdcpp}/c++11/cow-stdexcept.cc
+	${libstdcpp}/c++11/ctype.cc
+	${libstdcpp}/c++11/debug.cc
+	${libstdcpp}/c++11/functexcept.cc
+	${libstdcpp}/c++11/functional.cc
+	${libstdcpp}/c++11/futex.cc
+	${libstdcpp}/c++11/future.cc
+	${libstdcpp}/c++11/hash_c++0x.cc
+	${libstdcpp}/c++11/hashtable_c++0x.cc
+	${libstdcpp}/c++11/ios.cc
+	${libstdcpp}/c++11/limits.cc
+	${libstdcpp}/c++11/mutex.cc
+	${libstdcpp}/c++11/placeholders.cc
+	${libstdcpp}/c++11/random.cc
+	${libstdcpp}/c++11/regex.cc 
+	${libstdcpp}/c++11/shared_ptr.cc
+	${libstdcpp}/c++11/snprintf_lite.cc
+	${libstdcpp}/c++11/system_error.cc
+	# ${libstdcpp}/c++11/thread.cc
+	${libstdcpp}/c++11/sso_string.cc
+
+	${libstdcpp}/c++11/ext11-inst.cc
+	${libstdcpp}/c++11/fstream-inst.cc
+	${libstdcpp}/c++11/ios-inst.cc
+	${libstdcpp}/c++11/iostream-inst.cc
+	${libstdcpp}/c++11/istream-inst.cc
+	${libstdcpp}/c++11/locale-inst.cc
+	${libstdcpp}/c++11/ostream-inst.cc
+	${libstdcpp}/c++11/sstream-inst.cc
+	${libstdcpp}/c++11/streambuf-inst.cc
+	${libstdcpp}/c++11/string-inst.cc
+	${libstdcpp}/c++11/string-io-inst.cc
+	${libstdcpp}/c++11/wlocale-inst.cc
+	${libstdcpp}/c++11/wstring-inst.cc
+	${libstdcpp}/c++11/wstring-io-inst.cc
 )
 
 set(LIBSTDCPP_C17_SOURCES
@@ -71,7 +80,7 @@ set(LIBSTDCPP_C17_SOURCES
 	# ${libstdcpp}/c++17/fs_ops.cc
 	# ${libstdcpp}/c++17/fs_path.cc
 	${libstdcpp}/c++17/memory_resource.cc
-	${libstdcpp}/c++17/ostream-inst.cc
+	# ${libstdcpp}/c++17/ostream-inst.cc
 	${libstdcpp}/c++17/string-inst.cc
 )
 
@@ -89,11 +98,20 @@ set(LIBSTDCPP_SHARED_SOURCES
 	${libstdcpp}/shared/hashtable-aux.cc
 )
 
+add_library(libstdcpp98 OBJECT ${LIBSTDCPP_C98_SOURCES})
+target_compile_options(libstdcpp98 PRIVATE -std=c++11)
+
+add_library(libstdcpp11 OBJECT ${LIBSTDCPP_C11_SOURCES})
+target_compile_options(libstdcpp11 PRIVATE -std=c++11)
+
+
 target_sources(metamodule-plugin-libc PRIVATE
-    ${LIBCPP_C98_SOURCES}
-    ${LIBCPP_C11_SOURCES}
-    ${LIBCPP_C17_SOURCES}
-    ${LIBCPP_C20_SOURCES}
-    ${LIBSTDCPP_FILESYSTEM_SOURCES}
+    # ${LIBSTDCPP_C98_SOURCES}
+    # ${LIBSTDCPP_C11_SOURCES}
+    ${LIBSTDCPP_C17_SOURCES}
+    # ${LIBSTDCPP_C20_SOURCES}
+    # ${LIBSTDCPP_FILESYSTEM_SOURCES}
     ${LIBSTDCPP_SHARED_SOURCES}
 )
+
+target_link_libraries(metamodule-plugin-libc PUBLIC libstdcpp98 libstdcpp11)
