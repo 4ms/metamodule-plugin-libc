2b324440806a5b1899394bfc6076ac5072200d2e
Dan Green <danngreen1@gmail.com>
2024-05-01 22:47:18 -0700

    WIP build libstdc++ with flags from Makefile.in

--- libstdcpp.cmake
+++ libstdcpp.cmake
@@ -1,4 +1,46 @@
 set(libstdcpp ${CMAKE_CURRENT_LIST_DIR}/libstdc++-v3/src)
+set(libstdcpproot ${CMAKE_CURRENT_LIST_DIR}/libstdc++-v3)
+
+#TODO: split this up into files for each c++ version:
+
+##
+## c++98
+##
+
+#used when ENABLE_DUAL_API
+# except.. these are the same as the HOST_SOURCES?
+# set(LIBSTDCPP_C98_COW_STRING_HOST_SOURCES
+# 	${libstdcpp}/collate_members_cow.cc
+# 	${libstdcpp}/messages_members_cow.cc
+# 	${libstdcpp}/monetary_members_cow.cc
+# 	${libstdcpp}/numeric_members_cow.cc
+# )
+
+set(LIBSTDCPP_C98_HOST_SOURCES
+	# ${libstdcpp}/../config/locale/gnu/codecvt_members.cc
+	# ${libstdcpp}/../config/locale/gnu/collate_members.cc
+	# ${libstdcpp}/../config/locale/gnu/messages_members.cc
+	${libstdcpp}/../config/locale/gnu/monetary_members.cc
+	# ${libstdcpp}/../config/locale/gnu/numeric_members.cc
+	# ${libstdcpp}/../config/locale/gnu/time_members.cc
+)
+
+set(LIBSTDCPP_C98_HOST_SOURCES_EXTRA
+	${libstdcpp}/../config/locale/generic/c_locale.cc
+	# ${libstdcpp}/../config/locale/gnu/c_locale.cc
+	${libstdcpp}/../config/io/basic_file_stdio.cc
+	#inst_sources:
+	${libstdcpp}/c++98/allocator-inst.cc
+	${libstdcpp}/c++98/concept-inst.cc
+	${libstdcpp}/c++98/ext-inst.cc
+	${libstdcpp}/c++98/misc-inst.cc
+	#parallel_sources:
+	${libstdcpp}/c++98/parallel_settings.cc
+)
+
+set(LIBSTDCPP_C98_CXX11_ABI_SOURCES
+	${libstdcpp}/c++98/cow-istream-string.cc
+)
 
 set(LIBSTDCPP_C98_SOURCES
 	${libstdcpp}/c++98/bitmap_allocator.cc
@@ -11,16 +53,16 @@ set(LIBSTDCPP_C98_SOURCES
 	${libstdcpp}/c++98/hashtable_tr1.cc
 	${libstdcpp}/c++98/ios_failure.cc
 	${libstdcpp}/c++98/ios_init.cc
-	# ${libstdcpp}/c++98/ios_locale.cc
+	${libstdcpp}/c++98/ios_locale.cc 
 	${libstdcpp}/c++98/list.cc
 	${libstdcpp}/c++98/list-aux.cc
 	${libstdcpp}/c++98/list-aux-2.cc
 	${libstdcpp}/c++98/list_associated.cc
 	${libstdcpp}/c++98/list_associated-2.cc
 	${libstdcpp}/c++98/locale.cc
-	${libstdcpp}/c++98/locale_init.cc
+	# ${libstdcpp}/c++98/locale_init.cc #moved to c++11 (see TODO in c++98/Makefile.in)
 	${libstdcpp}/c++98/locale_facets.cc
-	${libstdcpp}/c++98/localename.cc
+	# ${libstdcpp}/c++98/localename.cc #moved to c++11 (see TODO in c++98/Makefile.in)
 	${libstdcpp}/c++98/math_stubs_float.cc
 	${libstdcpp}/c++98/math_stubs_long_double.cc
 	${libstdcpp}/c++98/stdexcept.cc
@@ -30,9 +72,87 @@ set(LIBSTDCPP_C98_SOURCES
 	${libstdcpp}/c++98/istream-string.cc
 	${libstdcpp}/c++98/streambuf.cc
 	${libstdcpp}/c++98/valarray.cc
+
+	${LIBSTDCPP_C98_CXX11_ABI_SOURCES}
+	${LIBSTDCPP_C98_HOST_SOURCES}
+	${LIBSTDCPP_C98_HOST_SOURCES_EXTRA}
+)
+
+add_library(libstdcpp98 OBJECT ${LIBSTDCPP_C98_SOURCES})
+
+target_include_directories(libstdcpp98 PRIVATE 
+	${libstdcpproot}/config/locale/gnu
+	${CMAKE_CURRENT_LIST_DIR}/include
+)
+target_compile_options(libstdcpp98 PRIVATE -std=gnu++98)
+
+set_source_files_properties(
+    ${libstdcpp}/c++98/locale_init.cc 
+    ${libstdcpp}/c++98/localename.cc 
+    PROPERTIES COMPILE_OPTIONS "-fchar8_t"
+)
+
+set_source_files_properties(
+    ${libstdcpp}/c++98/concept-inst.cc 
+    PROPERTIES COMPILE_OPTIONS "-fimplicit-templates"
+)
+
+set_source_files_properties(
+    ${libstdcpp}/c++98/strstream.cc 
+    ${libstdcpp}/c++98/bitmap_allocator.cc 
+	PROPERTIES COMPILE_OPTIONS "-Wno-deprecated;-I${libstdcpproot}/include/backward"
+)
+
+set_source_files_properties(
+    ${libstdcpp}/c++98/parallel_settings.cc 
+	PROPERTIES COMPILE_OPTIONS "-D_GLIBCXX_PARALLEL"
+)
+
+##
+## c++11 
+##
+
+# See libstdc++v3/src/c++11/Makefile.am:
+
+set(LIBSTDCPP_C11_HOST_SOURCES
+	#newlib:
+	${libstdcpp}/../config/os/newlib/ctype_configure_char.cc 
+	${libstdcpp}/../config/locale/newlib/ctype_members.cc
+	#generic
+	# ${libstdcpproot}/config/os/generic/ctype_configure_char.cc 
+	# ${libstdcpproot}/config/locale/generic/ctype_members.cc
+
+)
+
+#used when ENABLE_DUAL_API
+set(LIBSTDCPP_C11_CXX11_ABI_SOURCES
+	#cxx11_abi_sources:
+	${libstdcpp}/c++11/cow-locale_init.cc
+	${libstdcpp}/c++11/cow-shim_facets.cc
+	${libstdcpp}/c++11/cxx11-hash_tr1.cc
+	${libstdcpp}/c++11/cxx11-ios_failure.cc
+	${libstdcpp}/c++11/cxx11-shim_facets.cc
+	${libstdcpp}/c++11/cxx11-stdexcept.cc
+)
+
+#used when ENABLE_DUAL_API
+set(LIBSTDCPP_C11_EXTRA_STRING_INST_SOURCES
+	# extra_string_inst_sources:
+	${libstdcpp}/c++11/cow-fstream-inst.cc
+	${libstdcpp}/c++11/cow-sstream-inst.cc
+	${libstdcpp}/c++11/cow-string-inst.cc
+	${libstdcpp}/c++11/cow-string-io-inst.cc
+	${libstdcpp}/c++11/cow-wstring-inst.cc
+	${libstdcpp}/c++11/cow-wstring-io-inst.cc
+	${libstdcpp}/c++11/cxx11-locale-inst.cc
+	${libstdcpp}/c++11/cxx11-wlocale-inst.cc
+	${libstdcpp}/c++11/sso_string.cc
 )
 
 set(LIBSTDCPP_C11_SOURCES
+	${libstdcpp}/c++98/locale_init.cc 
+	${libstdcpp}/c++98/localename.cc
+
 	${libstdcpp}/c++11/chrono.cc
 	${libstdcpp}/c++11/codecvt.cc
 	${libstdcpp}/c++11/condition_variable.cc
@@ -55,8 +175,12 @@ set(LIBSTDCPP_C11_SOURCES
 	${libstdcpp}/c++11/snprintf_lite.cc
 	${libstdcpp}/c++11/system_error.cc
 	# ${libstdcpp}/c++11/thread.cc
-	${libstdcpp}/c++11/sso_string.cc
 
+	${LIBSTDCPP_C11_CXX11_ABI_SOURCES}
+	${LIBSTDCPP_C11_HOST_SOURCES}
+
+	#these are enabled if ENABLE_EXTERN_TEMPLATE
+	${LIBSTDCPP_C11_EXTRA_STRING_INST_SOURCES}
 	${libstdcpp}/c++11/ext11-inst.cc
 	${libstdcpp}/c++11/fstream-inst.cc
 	${libstdcpp}/c++11/ios-inst.cc
@@ -73,6 +197,34 @@ set(LIBSTDCPP_C11_SOURCES
 	${libstdcpp}/c++11/wstring-io-inst.cc
 )
 
+add_library(libstdcpp11 OBJECT ${LIBSTDCPP_C11_SOURCES})
+
+target_include_directories(libstdcpp11 PRIVATE 
+	${libstdcpproot}/config/locale/gnu
+	${CMAKE_CURRENT_LIST_DIR}/include
+)
+
+target_compile_options(libstdcpp11 PRIVATE -std=gnu++11)
+
+set_source_files_properties(
+	${libstdcpp}/c++11/hashtable_c++0x.cc
+    PROPERTIES COMPILE_OPTIONS "-fimplicit-templates"
+)
+
+set_source_files_properties(
+	${libstdcpp}/c++11/codecvt.cc
+	${libstdcpp}/c++11/limits.cc
+    PROPERTIES COMPILE_OPTIONS "-fchar8_t"
+)
+
+# c++11/Makefile.in does some regex on the assembly of cxx11-ios_failure.cc :
+#$(CXXCOMPILE) -gno-as-loc-support -S cxx11-ios_failure.cc -o tmp-$@
+#sed -e '/^_*_ZTISt13__ios_failure:/,/_ZTVN10__cxxabiv120__si_class_type_infoE/s/_ZTVN10__cxxabiv120__si_class_type_infoE/_ZTVSt19__iosfail_type_info/' tmp-$@ > cxx11-ios_failure.s
+
+#
+# c++17
+# 
+
 set(LIBSTDCPP_C17_SOURCES
 	${libstdcpp}/c++17/floating_from_chars.cc
 	${libstdcpp}/c++17/floating_to_chars.cc
@@ -80,9 +232,26 @@ set(LIBSTDCPP_C17_SOURCES
 	# ${libstdcpp}/c++17/fs_ops.cc
 	# ${libstdcpp}/c++17/fs_path.cc
 	${libstdcpp}/c++17/memory_resource.cc
-	# ${libstdcpp}/c++17/ostream-inst.cc
+	${libstdcpp}/c++17/ostream-inst.cc
 	${libstdcpp}/c++17/string-inst.cc
+	${libstdcpp}/c++17/cow-string-inst.cc
+
+	# ${libstdcpp}/c++17/cow-fs_dir.cc
+	# ${libstdcpp}/c++17/cow-fs_ops.cc
+	# ${libstdcpp}/c++17/cow-fs_path.cc
 )
+
+add_library(libstdcpp17 OBJECT ${LIBSTDCPP_C17_SOURCES})
+
+target_compile_options(libstdcpp17 PRIVATE 
+	-std=gnu++17 
+	# -nostdinc++ 
+	-fimplicit-templates
+)
+
+#
+# c++20
+#
 
 set(LIBSTDCPP_C20_SOURCES
 	${libstdcpp}/c++20/sstream-inst.cc
@@ -98,11 +267,28 @@ set(LIBSTDCPP_SHARED_SOURCES
 	${libstdcpp}/shared/hashtable-aux.cc
 )
 
-add_library(libstdcpp98 OBJECT ${LIBSTDCPP_C98_SOURCES})
-target_compile_options(libstdcpp98 PRIVATE -std=c++11)
 
-add_library(libstdcpp11 OBJECT ${LIBSTDCPP_C11_SOURCES})
-target_compile_options(libstdcpp11 PRIVATE -std=c++11)
+add_library(libstdcpp20 OBJECT 
+	${LIBSTDCPP_C20_SOURCES}
+	# ${LIBSTDCPP_FILESYSTEM_SOURCES} # filesystem not used in MetaModule
+)
+
+target_compile_options(libstdcpp20 PRIVATE -std=gnu++20)
+
+#
+#
+#
+
+target_sources(metamodule-plugin-libc PRIVATE
+    ${LIBSTDCPP_SHARED_SOURCES}
+)
+
+target_link_libraries(metamodule-plugin-libc PUBLIC 
+	libstdcpp98 
+	libstdcpp11 
+	libstdcpp17 
+	libstdcpp20
+)
 
 # Export include dirs so we can override system headers we want 
 # to disable (like iostreams)
@@ -110,17 +296,8 @@ target_include_directories(metamodule-plugin-libc PUBLIC
 	${CMAKE_CURRENT_LIST_DIR}/include
 )
 
-target_sources(metamodule-plugin-libc PRIVATE
-    ${LIBSTDCPP_C17_SOURCES}
-	# ${LIBSTDCPP_C20_SOURCES} # sstream not used in MetaModule
-	# ${LIBSTDCPP_FILESYSTEM_SOURCES} # filesystem not used in MetaModule
-    ${LIBSTDCPP_SHARED_SOURCES}
-)
-set_source_files_properties(
-    ${libstdcpp}/c++98/strstream.cc 
-    ${libstdcpp}/c++98/bitmap_allocator.cc 
-    PROPERTIES COMPILE_OPTIONS "-Wno-deprecated"
+target_compile_definitions(metamodule-plugin-libc PRIVATE 
+	__HAVE_LOCALE_INFO__
+	__HAVE_LOCALE_INFO_EXTENDED__
 )
 
-
-target_link_libraries(metamodule-plugin-libc PUBLIC libstdcpp98 libstdcpp11)
