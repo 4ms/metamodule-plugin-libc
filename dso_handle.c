9cb287109f129a747f5edb4ba0511fa22ef3056e
Dan Green <danngreen1@gmail.com>
2024-08-26 14:11:51 -0700

    Temporarily stub out getentropy()

--- dso_handle.c
+++ dso_handle.c
@@ -1,2 +1,11 @@
 void *ImageHandle;
 const void *const __dso_handle = &ImageHandle;
+
+int getentropy(void *buffer, unsigned length) {
+
+  char *b = buffer;
+
+  for (unsigned i = 0; i < length; i++)
+    *b++ = 1;
+  return 0;
+}
